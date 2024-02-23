9d9286bde86543f99de896be69d059ac82ca130c
Nati402K7 <natnaeleyasu402@gmail.com>
2024-02-23 14:32:18 -0800

    done ayyyyy

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - A function that run an infinite while loop.
+ * Return: 0
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - A function that creates five zombie processes.
+ * Return: 0
+ */
+
+int main(void)
+{
+	pid_t k;
+	char i = 0;
+
+	while (i < 5)
+	{
+		k = fork();
+		if (k > 0)
+		{
+			printf("Zombie process created, PID: %d\n", k);
+			sleep(1);
+			i++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (EXIT_SUCCESS);
+}
---
+++ 0x05-processes_and_signals/README.md
@@ -0,0 +1,12 @@
+What is my PID
+List your processes
+Show your Bash PID
+Show your Bash PID made easy
+TO infinity and beyound
+Don't stop me now!
+stop me if you can
+Highlander
+Beheaded process
+Process and PID file
+Manage my process
+Zombie
